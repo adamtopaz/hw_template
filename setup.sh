d993a774a4ada6e021ca288dadf900c809b1491d
adamtopaz <github@adamtopaz.com>
2023-08-08 14:22:12 -0600

    try again

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 curl -sSfL https://github.com/leanprover/elan/releases/download/v1.4.2/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
 ./elan-init -y --default-toolchain none
-source $HOME/.profile
+echo "$HOME/.elan/bin" >> $GITHUB_PATH
 lake exe cache get
